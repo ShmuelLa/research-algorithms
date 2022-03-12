99e1bc93ff8379b349dce815099eff26b0fddce6
ShmueLa <shmuel516@gmail.com>
2022-03-12 23:31:48 +0200

    hw3 - ex1

---
+++ HW3/EX1/doc_to_html.py
@@ -0,0 +1,63 @@
+import sys
+import importlib
+
+"""
+    EX1 of HW3
+    Receives input file of a python module and an output file for the desired HTML file 
+    from the users commandline and extract the documentation of the module into an HTML
+    include minimal file error handling
+"""
+
+
+def generate_html_from_module(module):
+    """
+    Extracts a module documentations from a module object into a HTML string
+    uses a pre-written builtins list in order to exclude built in functions
+
+    :param module: Module object type to extract documentation from
+    :return: String representation of an HTML file
+    """
+    builtins_lst = ['__name__',
+                    '__doc__',
+                    '__package__',
+                    '__loader__',
+                    '__spec__',
+                    '__file__',
+                    '__cached__',
+                    '__builtins__']
+    html_content = f"<html><head><title>{module.__name__} Doc</title></head><body><h1>Module {module.__name__}:</h1>"
+    html_content += f"Function {module.__doc__}"
+    for function in module.__dict__:
+        if function not in builtins_lst:
+            html_content += f"<h2>Function {function}:</h2>"
+            html_content += f"{getattr(module, function).__doc__}"
+            html_content += f"<h3>Annotations:</h3>"
+            for annotation in getattr(module, function).__annotations__.keys():
+                html_content += f"{annotation} <br>"
+    return html_content
+
+
+def doc_to_html():
+    """
+    Gets the user commandline input and calls the html creaton function
+    This method also handles file read write errors
+
+    :return: Creates a HTML file with the desired name and
+    """
+    try:
+        module_file_name = sys.argv[1]
+        output_html = sys.argv[2]
+    except IndexError or UnboundLocalError:
+        print("Error: Please enter two correct arguments at the right order [1. module file, 2. output HTML file]")
+        sys.exit(0)
+    module = importlib.import_module(module_file_name.replace(".py", ""))
+    try:
+        file = open(output_html, "w")
+        file.write(generate_html_from_module(module))
+        file.close()
+    except Exception as error:
+        print(error)
+
+
+if __name__ == "__main__":
+    doc_to_html()
