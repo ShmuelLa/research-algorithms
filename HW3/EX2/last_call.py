66d30688490d9111c5817c83184c04738ed9d636
ShmueLa <shmuel516@gmail.com>
2022-03-14 01:25:47 +0200

    added improved tests + minor improvements

---
+++ HW3/EX2/last_call.py
@@ -0,0 +1,46 @@
+last_val = None
+last_func = None
+
+
+def lastcall(func):
+    """
+    Checks it the same function is being called again with the same former input
+    If the case is true than prints the value is being repeated.
+    Else, runs the function and saves the input value
+    :param func:
+    :return:
+    """
+    def wrapper(*args):
+        global last_val, last_func
+        if last_func == func:
+            if last_val is None or last_val != args:
+                last_val = args
+            elif last_val == args:
+                print(f"I already told you that the answer is {func(*args)}!")
+        else:
+            last_func = func
+            last_val = args
+
+    return wrapper
+
+
+@lastcall
+def square_num(x):
+    return x**2
+
+
+@lastcall
+def add_two(x):
+    return x+2
+
+
+if __name__ == "__main__":
+    """
+    Test runs for checking
+    """
+    square_num(2)
+    square_num(2)
+    square_num(3)
+    square_num(3)
+    add_two(3)
+    add_two(3)
--- HW3/hw3_tests.py
+++ HW3/hw3_tests.py
@@ -1,7 +1,9 @@
 import unittest
 import os
-import inspect
-from HW3 import EX1
+from io import StringIO
+from unittest.mock import patch
+import HW3.EX3
+from HW3.EX2.last_call import square_num, reset, add_two
 
 
 def ex1_remove_all_html_files():
@@ -23,6 +25,9 @@ def run_ex1(working_directory, python_file_name: str, html_output: str, python_f
 
 
 class doc_to_html_test(unittest.TestCase):
+    """
+    Tests for EX1 - HTML documentation generator from python module
+    """
 
     def test_file_creation_and_naming(self):
         ex1_dir = ex1_remove_all_html_files()
@@ -31,11 +36,38 @@ class doc_to_html_test(unittest.TestCase):
         self.assertTrue("doc_test.html" in [file for file in os.listdir(ex1_dir)])
 
     def test_html_output_content(self):
-
         ex1_dir = ex1_remove_all_html_files()
         run_ex1(ex1_dir, 'doc_to_html.py', 'doc_test.html', 'homeworkmodule.py')
         with open(os.path.join(ex1_dir, 'doc_test.html'), "r") as file:
             html_content = file.read()
+
+
+class last_call_test(unittest.TestCase):
+    """
+    Tests for EX2 - last call input module
+    """
+
+    def test_square_func(self):
+        with patch('sys.stdout', new=StringIO()) as test_output:
+            square_num(2)
+            self.assertEqual(test_output.getvalue(), "")
+            square_num(2)
+            self.assertTrue("4" in test_output.getvalue())
+        with patch('sys.stdout', new=StringIO()) as test_output:
+            square_num(3)
+            self.assertEqual(test_output.getvalue(), "")
+            square_num(3)
+            self.assertTrue("9" in test_output.getvalue())
+
+    def test_function_switch(self):
+        with patch('sys.stdout', new=StringIO()) as test_output:
+            square_num(2)
+            self.assertEqual(test_output.getvalue(), "")
+            add_two(5)
+            self.assertEqual(test_output.getvalue(), "")
+            add_two(5)
+            self.assertTrue("7" in test_output.getvalue())
+
 
 
 if __name__ == "__main__":
