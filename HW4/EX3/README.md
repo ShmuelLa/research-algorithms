12554560385ff1568762ed15083e8e35c381bd7c
ShmueLa <shmuel516@gmail.com>
2022-04-06 06:19:30 +0300

    Added CodinGames Super Computer solution

---
+++ HW4/EX3/README.md
@@ -0,0 +1,11 @@
+CodinGames site profile:
+https://www.codingame.com/profile/efd53beabc4e1bf6420e8eca3f6ecf7d8907854
+
+Super Computer Solution (Hard)
+
+Challenge Link:
+https://www.codingame.com/ide/puzzle/super-computer
+Screenshots:
+![alt text](scrn1.jpg)
+![alt text](scrn2.jpg)
+![alt text](scrn3.jpg)
--- HW4/EX3/super_computer.py
+++ HW4/EX3/super_computer.py
@@ -4,23 +4,28 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
-experiments = {}
-n = int(input())
-for i in range(n):
+experiments = []
+result = 0
+last_expr = 0
+for i in range(int(input())):
     j, d = [int(j) for j in input().split()]
-    experiments[j] = d
-exp_count = 0
-start, end = None, None
-for k, v in sorted(experiments):
-    if start is None:
-        start, end = k, v
-        continue
-    if k < end:
-        exp_count += 1
-    start, end = k, v
+    # We will create a matrix of all the experiments
+    experiments.append((j, j + d))
+    last_expr = max(last_expr, experiments[i][1])
+# we will sort all the experiments by the second argument which is the end date
+experiments.sort(key=lambda exp: exp[1])
+# Initialize a timeline line
+timeline = [0] * last_expr
+for c in experiments:
+    # We check if there is a free timeline for the experiment
+    if 1 not in timeline[c[0]:c[1]]:
+        result += 1
+        # We mark the time as taken
+        timeline[c[0]:c[1]] = [1] * (c[1] - c[0])
 
-
+print(result)
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
-print(str(exp_count))
+
+
