43cab0e9c13ce9ddb9646f7539f1a865e86f9841
ShmueLa <shmuel516@gmail.com>
2022-06-12 03:37:58 +0300

    HW11 - added templates and iterations

--- HW11/pareto_flask.py
+++ HW11/pareto_flask.py
@@ -1,7 +1,4 @@
-from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import IntegerField, SubmitField
-from wtforms.validators import InputRequired, NumberRange
+from flask import Flask, render_template, request, redirect, url_for, flash
 import os
 
 SECRET_KEY = os.urandom(32)
@@ -12,30 +9,39 @@ app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    form = DataForm()
-    is_submitted = form.validate_on_submit()
-    # return render_template('dynamic-index.html')
-    return render_template('dynamic-index.html', form=form)
+    if request.method == 'POST':
+        agents_count = request.form.get('agents')
+        objects_count = request.form.get('objects')
+        if not request.form['agents'] or not request.form['objects']:
+            flash('Please fill all fields', 'error')
+        elif int(agents_count) < 1 or int(objects_count) < 1:
+            flash('Agents and objects must be larger or equal to 1', 'error')
+        else:
+            return redirect(url_for('allocation',
+                                    agents_count=int(agents_count),
+                                    objects_count=int(objects_count)))
+    return render_template('index.html')
 
 
-class DataForm(FlaskForm):
-    num_of_agents = IntegerField("Agents", validators=[InputRequired(), NumberRange(1, 5)])
-    num_of_objects = IntegerField("Objects", validators=[InputRequired(), NumberRange(2, 5)])
-    submit = SubmitField('Submit')
+@app.route('/allocation/<agents_count>/<objects_count>', methods=['GET', 'POST'])
+def allocation(agents_count, objects_count):
+    if request.method == 'POST':
+        user_input = request.form.getlist('ag_input')
+        user_input = ','.join(user_input)
+        return redirect(url_for('calculation',
+                                agents_count=int(agents_count),
+                                objects_count=int(objects_count),
+                                user_input=user_input))
+    return render_template('allocation.html', agents=int(agents_count), objects=int(objects_count))
 
-"""
-line 31 after:
-<div class="collapse navbar-collapse" id="navbar">
 
-<!--        <div class="navbar-nav">-->
-<!--          {% if user.is_authenticated %}-->
-<!--          <a class="nav-item nav-link" id="home" href="/">Home</a>-->
-<!--          <a class="nav-item nav-link" id="logout" href="/logout">Logout</a>-->
-<!--          {% else %}-->
-<!--          <a class="nav-item nav-link" id="login" href="/login">Login</a>-->
-<!--          <a class="nav-item nav-link" id="signUp" href="/sign-up">Sign Up</a>-->
-<!--          {% endif %}-->
-<!--        </div>-->
-"""
+@app.route('/calculation/<agents_count>/<objects_count>/<user_input>')
+def calculation(agents_count, objects_count, user_input):
+    return render_template('calculation.html',
+                           agents=int(agents_count),
+                           objects=int(objects_count),
+                           input=user_input)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
